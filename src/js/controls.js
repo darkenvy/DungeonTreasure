ac27c280b51843ef950aa5868f61cc1901b8ddb6
darkenvy <darkenvy6@gmail.com>
2016-06-06 17:30:03 -0700

    Music, Snakes, Score, Fuel All added and working

--- src/js/controls.js
+++ src/js/controls.js
@@ -36,14 +36,28 @@ function controls() {
   if (cursors.up.isDown && player.body.touching.down) {
     player.body.velocity.y = -450;
   }
+
+  if (cursors.down.isDown && bombs.children.length == 0 && timer > 5) {
+    timer -= 5;
+
+    var bomb = bombs.create( Math.floor(player.x/64)*64, (Math.floor(player.y/64)*64)-50, 'bomb');
+    bomb.smooth = false;
+    bomb.scale.x = 2;
+    bomb.scale.y = 2;
+    bomb.animations.add('lit', [0, 1, 2, 3, 4, 5, 6], 5, false);
+    bomb.animations.add('explode', [7, 8, 9, 10, 11, 12], 10, false);
+    bomb.play('lit');
+    // player.body.velocity.y = -450;
+  }
+
   // Jetpack activate
-  if (jetButton.isDown) {
-    // console.log(player.body.velocity.y);
+  if (jetButton.isDown && timer > 1) {
     if (player.body.velocity.y > -200) {
       jetVelocity = player.body.velocity.y - 50;
-      // console.log(jetVelocity);
-      // player.body.velocity.y = -200;
       player.body.velocity.y = jetVelocity ;
+      timer -= 0.3;
+      // timer = (Math.floor(timer * 10))/10;
+      timerText.text = timer;
 
     }
   }
